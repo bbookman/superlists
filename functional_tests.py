30272071c799144224b0b805797a59d59a2da692
Bruce Bookman <bruce.bookman@gmail.com>
2018-12-21 21:26:07 -0800

    test refactor

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,27 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+# Russell loves using Firefox and wants to check out the new todo app
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+#He calls up the todo app website
+    def test_can_open_todo_site(self):
+        self.browser.get('http://localhost:8000')
+#The todo site returns
+        self.assertIn('To-Do', self.browser.title)
+        self.fail()
+if __name__ == '__main__':
+    unittest.main(warnings = 'ignore')
+
+#He is presented with the opportunity to enter a todo
+
+#He types 'Buy an iPhone' and hits the Enter key
+
+#The page refreshes and he sees
+# 1. Buy an iPhone
